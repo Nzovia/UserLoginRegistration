eaa01954a72e798e188797dd6fef1d4d20d2cb3d
ADMIN <nickiemaundu19@gmail.com>
2021-09-22 09:44:42 +0300

    JPA test to evaluate successful user creation(userRepository- save method)

--- src/main/java/com/nicholas/userloginregistration/repository/UserRepository.java
+++ src/main/java/com/nicholas/userloginregistration/repository/UserRepository.java
@@ -4,4 +4,8 @@ import com.nicholas.userloginregistration.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    //this is the interface for the data access layer
+    //User is the domain class, and Long is type of the ID of the domain class
+
+
 }
---
+++ src/test/java/com/nicholas/userloginregistration/UserRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.nicholas.userloginregistration;
+
+import com.nicholas.userloginregistration.model.User;
+import com.nicholas.userloginregistration.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest// indicate that it is running as JPA test
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)//using real db
+@Rollback(value = false) // no rolling back the transactions
+public class UserRepositoryTests {
+    //in this class we test data access layer(Repository Interface)
+    @Autowired//to test user repository CRUD operations
+    private UserRepository repository;
+    @Autowired//to access the provided testmanager class by the SpringJPA used to perform assertion
+    private TestEntityManager entityManager;
+
+    //perform the firstTest, creating new users
+    @Test
+    public void testCreatUser(){
+        User user = new User();
+        user.setFirstName("Nicholas");
+        user.setSecondName("Maundu");
+        user.setEmail("nichonzovia@gmail.com");
+        user.setPassword("123$Nick");
+
+        //perform the CRUD operation save of the userRepository interface
+       User savedUser = repository.save(user);
+       //then to assert this test method we use entity manager
+       User exitUser = entityManager.find(User.class, savedUser.getId());// find entity by its primaryKey
+       //using the primary key get the user email to affirm it is as expected
+       assertThat(exitUser.getEmail()).isEqualTo(user.getEmail());
+
+    }
+}
